5a36bd8153e477d4d0da561782457ba79dc8900f
Ankur reyes <Ankur reyes>
2016-10-23 23:12:51 +0530

    5th commit

--- app.js
+++ app.js
@@ -1,7 +1,55 @@
-var Express = require("express");
-var app = Express();
-app.get("/",function(req,res){
-res.send("<html><h2>ankur</h2></html>");
+var express = require('express');
+var mongodb = require('mongodb');
+var path = require('path');
 
-})
+var MongoClient = mongodb.MongoClient;
+
+var mongo_url =  process.env.MONGOLAB_URI || "mongodb://ankur1163:lightbulb1@ds013946.mlab.com:13946/ank1163";
+
+var app = express();
+app.use(express.static(path.join(__dirname, 'views')));
+
 app.listen(process.env.PORT || 5000);
+app.get("/",function(req,res){
+  res.render("\views\index.html")
+})
+
+app.get("/process",function(req,res){
+Mongoclient.connect(mongo_url,function(err,db){
+  if(err){
+    console.log("there is error connecting mongodb");
+  }
+  else{
+    console.log("All went well with mongodb");
+  }
+})
+
+
+
+});
+
+/*
+
+app.get('/', function(req, res){
+    MongoClient.connect(mongo_url, function (err, db) {
+        // do inserts and quering here ...
+        if(err) {
+          throw err
+        }
+        var data = {
+            name: 'Vinay',
+            bio: 'web dev'
+        }
+        var insertedId = db.collection('demo').insert(data); // this is sync version of insertion
+        db.collection('demo').findOne({name: 'Vinay'}, function(error, doc){
+            if(error) throw error;
+            res.send(doc);
+        });
+        db.collection('demo').update({name: 'Vinay'},{$set: {bio: 'web developer'}}, function(error, doc){
+            if(error) throw error;
+            console.log(doc);
+        });
+
+    });
+});
+*/
