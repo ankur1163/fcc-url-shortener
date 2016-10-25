826d4daeefa86d01b9158ff6d03e69d9c5cb6647
vinaypuppal <me@vinaypuppal.com>
2016-10-25 19:15:36 +0530

    fixed few things

--- views/rest.js
+++ views/rest.js
@@ -1,9 +1,13 @@
-$('form').submit(function(event) {
-    event.preventDefault();
+$(document).ready(function(){
+  $('form').on('submit',function(e) {
+    e.preventDefault();
     var url = $('#url').val();
-
-    $.post('http://localhost:5000/process?' + $.param({url: url}), function() {
+    $('#short-url').text('Loading.......');
+    $.post('http://localhost:5000/process?' + $.param({url: url}), function(data) {
       $('input').val('');
       $('input').focus();
+      console.log(data);
+      $('#short-url').html('<strong>Short Url: </strong>'+data.short_url);
     });
   });
+});
